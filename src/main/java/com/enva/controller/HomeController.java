28c4266c62ad41f3893806651adb59a44d9a1c6f
DRAGU Costel <costel.dragu@endava.com>
2017-10-03 18:50:51 +0300

    Db Connections

--- src/main/java/com/enva/controller/HomeController.java
+++ src/main/java/com/enva/controller/HomeController.java
@@ -2,6 +2,8 @@ package com.enva.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @RequestMapping("/")
     public String home() {
-        logger.info("Home page loaded");
+        try {
+            int rowCount = this.jdbcTemplate.queryForObject("SELECT COUNT(*) from people", Integer.class);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+       logger.info("Home page loaded");
         return "Home Controller in place";
     }
 }
